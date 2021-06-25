0be2f86530997de6e45acec30359d8a7199f3dc3
Nasir <nasirlawal001@gmail.com>
2021-06-25 10:16:30 +0100

    create overview component

---
+++ src/components/Overview.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Overview
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Overview'
+})
+</script>
--- src/layouts/Index.vue
+++ src/layouts/Index.vue
@@ -1,5 +1,6 @@
 <template>
   <top-nav></top-nav>
+  <overview></overview>
   <services></services>
 </template>
 
@@ -7,11 +8,14 @@
 import { defineComponent } from 'vue';
 import TopNav from '../components/TopNav.vue';
 import Services from '../components/Services.vue';
+import Overview from '../components/Overview.vue';
+
 export default defineComponent({
   name: 'Index',
   components: {
     TopNav,
-    Services
+    Services,
+    Overview
   }
 })
 </script>
