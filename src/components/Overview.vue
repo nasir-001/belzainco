697507ff2d47245e3567a55ebd71ee0a30cd9d29
Nasir <nasirlawal001@gmail.com>
2021-06-25 12:46:20 +0100

    start adding responsive to the template

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="tw-mx-0">
-    <div class="tw-flex tw-justify-between tw-w-5/6 tw-mx-auto tw-mt-20">
-      <div class="tw-uppercase tw-w-1/2 tw-text-gray-700 tw-tracking-wide tw-font-serif tw-text-5xl tw-font-bold">
+    <div class="sm:tw-flex tw-justify-between tw-w-5/6 tw-mx-auto tw-mt-14 sm:tw-mt-20">
+      <div class="tw-uppercase sm:tw-w-1/2 tw-text-gray-700 sm:tw-tracking-wide tw-font-serif tw-text-2xl tw-text-center sm:tw-text-5xl tw-font-bold">
         belzainco global links nigerian limited
         <p class="tw-text-sm tw-mt-4 tw-text-gray-500 tw-font-sans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quo molestiae, vitae dolorum beatae dolores maiores ut architecto cum aperiam fuga. Quo nam maxime veritatis velit animi omnis voluptates! Rem?</p>
       </div>
-      <div class="tw-w-1/2">
+      <div class="sm:tw-w-1/2">
         <img class="tw-w-full" src="/img/images.jpg" alt="">
       </div>
     </div>
