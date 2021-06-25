00da12703895b198f84ddffd98276eba43110c28
Nasir <nasirlawal001@gmail.com>
2021-06-25 17:38:58 +0100

    change text breakpoint

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tw-mx-0">
     <div class="md:tw-flex tw-justify-between tw-w-5/6 tw-mx-auto tw-mt-14 md:tw-mt-10">
-      <div class="tw-uppercase md:tw-w-1/2 tw-text-gray-800 md:tw-tracking-wide tw-font-serif tw-text-3xl tw-font-thin md:tw-text-7xl">
+      <div class="tw-uppercase md:tw-w-1/2 tw-text-gray-800 md:tw-tracking-wide tw-font-serif tw-text-center md:tw-text-left tw-text-4xl tw-font-thin md:tw-text-7xl">
         belzainco global links nigerian limited
         <p class="md:tw-text-md tw-text-lg tw-text-gray-700 tw-tracking-normal tw-font-normal tw-ml-1 tw-mt-4 tw-font-mono">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quo molestiae, vitae dolorum beatae dolores maiores ut architecto cum aperiam fuga. Quo nam maxime veritatis velit animi omnis voluptates! Rem?</p>
       </div>
