9c2a07903608e5c4338db0673b8363415c6dfec7
Nasir <nasirlawal001@gmail.com>
2021-06-25 09:33:48 +0100

    edit the mobile list item

--- src/layouts/Index.vue
+++ src/layouts/Index.vue
@@ -13,45 +13,39 @@
       icon="menu"
       aria-label="Menu">
       <q-menu
-        transition-show="flip-right"
-        transition-hide="flip-left">
-        <q-list>
+        transition-show="scale"
+        transition-hide="scale">
+        <q-list style="min-width: 150px">
           <q-item clickable>
-            <q-item-section>New tab</q-item-section>
-          </q-item>
-          <q-item clickable>
-            <q-item-section>New incognito tab</q-item-section>
+            <q-item-section class="tw-text-sm tw-cursor-pointer tw-uppercase tw-text-red-600">Manager</q-item-section>
           </q-item>
           <q-separator />
           <q-item clickable>
-            <q-item-section>Recent tabs</q-item-section>
-          </q-item>
-          <q-item clickable>
-            <q-item-section>History</q-item-section>
-          </q-item>
-          <q-item clickable>
-            <q-item-section>Downloads</q-item-section>
+            <q-item-section class="tw-text-sm tw-cursor-pointer tw-uppercase tw-text-red-600">Services</q-item-section>
           </q-item>
           <q-separator />
           <q-item clickable>
-            <q-item-section>Settings</q-item-section>
+            <q-item-section class="tw-text-sm tw-cursor-pointer tw-uppercase tw-text-red-600">Products</q-item-section>
           </q-item>
           <q-separator />
           <q-item clickable>
-            <q-item-section>Help &amp; Feedback</q-item-section>
+            <q-item-section class="tw-text-sm tw-cursor-pointer tw-uppercase tw-text-red-600">Aims</q-item-section>
           </q-item>
         </q-list>
       </q-menu>
     </q-btn>
     <div class="sm:tw-flex tw-hidden tw-justify-evenly tw--mt-2">
       <q-item class="tw-text-red-600 tw-text-lg" clickable>
-        <q-item-section>History</q-item-section>
+        <q-item-section>Manager</q-item-section>
       </q-item>
       <q-item class="tw-text-red-600 tw-text-lg" clickable>
-        <q-item-section>History</q-item-section>
+        <q-item-section>Services</q-item-section>
       </q-item>
       <q-item class="tw-text-red-600 tw-text-lg" clickable>
-        <q-item-section>History</q-item-section>
+        <q-item-section>Products</q-item-section>
+      </q-item>
+      <q-item class="tw-text-red-600 tw-text-lg" clickable>
+        <q-item-section>Aims</q-item-section>
       </q-item>
     </div>
   </div>
