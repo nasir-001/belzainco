632bf853d9df97f4ff206b02a8675c5898ad66d0
Nasir <nasirlawal001@gmail.com>
2021-06-25 18:14:11 +0100

    sizing company name in overview component

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -3,7 +3,7 @@
     <div class="md:tw-flex tw-justify-between tw-w-5/6 tw-mx-auto tw-mt-14 md:tw-mt-10">
       <div class="tw-uppercase md:tw-w-1/2 tw-text-gray-800 md:tw-tracking-wide tw-font-serif tw-text-center md:tw-text-left tw-text-4xl sm:tw-text-6xl tw-font-thin md:tw-text-7xl">
         belzainco global links nigerian limited
-        <p class="md:tw-text-md tw-text-lg tw-text-gray-700 tw-tracking-normal tw-font-normal tw-ml-1 tw-mt-4 tw-font-mono">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quo molestiae, vitae dolorum beatae dolores maiores ut architecto cum aperiam fuga. Quo nam maxime veritatis velit animi omnis voluptates! Rem?</p>
+        <p class="md:tw-text-md tw-text-lg tw-text-gray-700 tw-tracking-normal tw-font-thin tw-ml-1 tw-mt-4 tw-font-mono">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quo molestiae, vitae dolorum beatae dolores maiores ut architecto cum aperiam fuga. Quo nam maxime veritatis velit animi omnis voluptates! Rem?</p>
       </div>
       <div class="md:tw-w-1/2 md:tw--mt-20">
         <!-- <img class="tw-w-full" src="/img/images.jpg" alt=""> -->
