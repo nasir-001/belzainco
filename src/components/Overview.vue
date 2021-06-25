ad78a47d7935be0093cd024617643d0f73fa43c6
Nasir <nasirlawal001@gmail.com>
2021-06-25 12:26:05 +0100

    add the two flex items

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="tw-mx-0">
     <div class="tw-flex tw-justify-between tw-w-5/6 tw-mx-auto tw-mt-20">
-      <div class="tw-uppercase tw-w-1/2 tw-text-gray-700 tw-text-5xl tw-font-bold">
+      <div class="tw-uppercase tw-w-1/2 tw-text-gray-700 tw-tracking-wide tw-font-serif tw-text-5xl tw-font-bold">
         belzainco global links nigerian limited
+        <p class="tw-text-sm tw-mt-4 tw-text-gray-500 tw-font-sans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quo molestiae, vitae dolorum beatae dolores maiores ut architecto cum aperiam fuga. Quo nam maxime veritatis velit animi omnis voluptates! Rem?</p>
+      </div>
+      <div class="tw-w-1/2">
+        <img class="tw-w-full" src="/img/images.jpg" alt="">
       </div>
     </div>
   </div>
