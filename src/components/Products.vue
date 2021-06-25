c9aab12475b51d3f54f760ca55a9c5ee78d9211a
Nasir <nasirlawal001@gmail.com>
2021-06-25 21:20:49 +0100

    add flex with 4 cards

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
     <div class="tw-text-3xl tw-text-center tw-text-gray-800 tw-font-mono">
-      Our Services
+      Our Products
+    </div>
+    <div class="tw-flex tw-justify-evenly tw-mx-6 tw-mt-6">
+      <div class="tw-bg-red-50 tw-w-1/4 tw-mx-2">
+        first
+      </div>
+      <div class="tw-bg-red-50 tw-w-1/4 tw-mx-2">
+        first
+      </div>
+      <div class="tw-bg-red-50 tw-w-1/4 tw-mx-2">
+        first
+      </div>
+      <div class="tw-bg-red-50 tw-w-1/4 tw-mx-2">
+        first
+      </div>
     </div>
   </div>
 </template>
