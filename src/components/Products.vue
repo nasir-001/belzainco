b34807b90c4a83e4dff1fbeec6a5216b4951605d
Nasir <nasirlawal001@gmail.com>
2021-06-25 20:58:53 +0100

    create products component

---
+++ src/components/Products.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Products
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Products'
+})
+</script>
--- src/layouts/Index.vue
+++ src/layouts/Index.vue
@@ -2,6 +2,7 @@
   <top-nav></top-nav>
   <overview></overview>
   <services></services>
+  <products></products>
 </template>
 
 <script>
@@ -9,13 +10,15 @@ import { defineComponent } from 'vue';
 import TopNav from '../components/TopNav.vue';
 import Services from '../components/Services.vue';
 import Overview from '../components/Overview.vue';
+import Products from '../components/Products.vue';
 
 export default defineComponent({
   name: 'Index',
   components: {
     TopNav,
     Services,
-    Overview
+    Overview,
+    Products
   }
 })
 </script>
