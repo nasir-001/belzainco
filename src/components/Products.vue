12788a98358e1c7c583d4a6257c88d09ce284309
Nasir <nasirlawal001@gmail.com>
2021-06-25 21:53:11 +0100

    add flex item to the products component

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -3,18 +3,22 @@
     <div class="tw-text-2xl sm:tw-text-3xl md:tw-text-4xl tw-text-center tw-text-gray-800 tw-font-mono">
       Our Products
     </div>
-    <div class="tw-flex tw-justify-evenly tw-mx-6 tw-mt-6">
-      <div class="tw-bg-red-50 tw-w-1/4 tw-mx-2">
-        first
+    <div class="tw-flex tw-justify-evenly tw-mx-28 tw-mt-6 tw-mb-10">
+      <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
+        <img class="tw-object-contain" src="/img/images.jpg" alt="">
+        <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2">mango</p>
       </div>
-      <div class="tw-bg-red-50 tw-w-1/4 tw-mx-2">
-        first
+      <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
+        <img class="tw-object-contain" src="/img/images.jpg" alt="">
+        <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2">mango</p>
       </div>
-      <div class="tw-bg-red-50 tw-w-1/4 tw-mx-2">
-        first
+      <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
+        <img class="tw-object-contain" src="/img/images.jpg" alt="">
+        <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2">mango</p>
       </div>
-      <div class="tw-bg-red-50 tw-w-1/4 tw-mx-2">
-        first
+      <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
+        <img class="tw-object-contain" src="/img/images.jpg" alt="">
+        <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2">mango</p>
       </div>
     </div>
   </div>
