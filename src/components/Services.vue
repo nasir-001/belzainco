7cad317085ad14a37263bab5cd93df04523d792c
Nasir <nasirlawal001@gmail.com>
2021-06-26 23:40:29 +0100

    start adding responsive to the cards

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -3,7 +3,13 @@
     <div class="tw-text-2xl sm:tw-text-3xl md:tw-text-4xl tw-text-center tw-text-gray-800 tw-font-mono">
       Services we Provide
     </div>
-    <div class="tw-flex tw-mx-auto tw-justify-evenly tw-mt-10">
+    <div class="tw-flex sm:tw-flex-wrap tw-mx-auto tw-justify-evenly tw-mt-10">
+      <div class="tw-w-1/3 tw-text-xl">
+        <service-card></service-card>
+      </div>
+      <div class="tw-w-1/3 tw-text-xl">
+        <service-card></service-card>
+      </div>
       <div class="tw-w-1/3 tw-text-xl">
         <service-card></service-card>
       </div>
