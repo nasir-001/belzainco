d2bd52e97aa37630aa5b3423f383ef9cf008fab1
Nasir <nasirlawal001@gmail.com>
2021-06-26 12:30:59 +0100

    add border to the product card

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -4,16 +4,16 @@
       Our Products
     </div>
     <div class="tw-flex tw-mt-20 tw-justify-evenly tw-mx-28 tw-mb-10">
-      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
+      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-border-2 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
         <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-naman-nayar-922764.jpg" alt="">
       </div>
-      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
+      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-border-2 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
         <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-naman-nayar-922764.jpg" alt="">
       </div>
-      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
+      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-border-2 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
         <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-naman-nayar-922764.jpg" alt="">
       </div>
-      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
+      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-border-2 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
         <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-naman-nayar-922764.jpg" alt="">
       </div>
     </div>
