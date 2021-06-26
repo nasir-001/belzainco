39354c1c8ccbf7f5e38b8335ae0a2639c179fb0c
Nasir <nasirlawal001@gmail.com>
2021-06-26 11:34:39 +0100

    remove al the products names from the crad

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -6,19 +6,15 @@
     <div class="tw-flex tw-justify-evenly tw-mx-28 tw-mt-6 tw-mb-10">
       <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
         <img class="tw-object-cover tw-opacity-100 hover:tw-opacity-80 tw-h-52 tw-w-full"  src="/img/pexels-ready-made-3987286.jpg" alt="">
-        <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2 hover:tw-text-gray-800 tw-font-mono tw-tracking-wider">mango</p>
       </div>
       <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
         <img class="tw-object-cover tw-opacity-100 hover:tw-opacity-80 tw-h-52 tw-w-full"  src="/img/pexels-ready-made-3850652.jpg" alt="">
-        <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2 hover:tw-text-gray-800 tw-font-mono tw-tracking-wider">mango</p>
       </div>
       <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
         <img class="tw-object-cover tw-opacity-100 hover:tw-opacity-80 tw-h-52 tw-w-full"  src="/img/pexels-pixabay-39303.jpg" alt="">
-        <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2 hover:tw-text-gray-800 tw-font-mono tw-tracking-wider">mango</p>
       </div>
       <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
         <img class="tw-object-cover tw-opacity-100 hover:tw-opacity-80 tw-h-52 tw-w-full"  src="/img/pexels-naman-nayar-922764.jpg" alt="">
-        <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2 hover:tw-text-gray-800 tw-font-mono tw-tracking-wider">mango</p>
       </div>
     </div>
   </div>
