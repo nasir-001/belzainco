f9851b9b5021bc2e41a68cc1a7c6d968998492e3
Nasir <nasirlawal001@gmail.com>
2021-06-26 21:49:05 +0100

    create card component

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    Services
+  <div class="tw-mx-32 tw-my-10">
+    <div class="tw-text-2xl sm:tw-text-3xl md:tw-text-4xl tw-text-center tw-text-gray-800 tw-font-mono">
+      Services we Provide
+    </div>
+    <div class="tw-flex tw-mx-auto tw-justify-evenly tw-mt-10">
+      <div class="tw-w-1/3 tw-text-xl tw-text-gray-700 tw-font-mono tw-uppercase">
+        agro processing
+      </div>
+      <div class="tw-w-1/3 tw-text-xl tw-text-gray-700 tw-font-mono tw-uppercase">
+        ginger exporting
+      </div>
+      <div class="tw-w-1/3 tw-text-xl tw-text-gray-700 tw-font-mono tw-uppercase">
+        sweet potato flour
+      </div>
+    </div>
   </div>
 </template>
 
