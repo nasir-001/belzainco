6393345e0bcd34b37626527f7ae7b8a3403997f4
Nasir <nasirlawal001@gmail.com>
2021-06-26 14:35:11 +0100

    edit the overview component

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,16 +1,14 @@
 <template>
-  <div>
-    <div class="lg:tw-flex tw-justify-between tw-w-5/6 tw-mx-auto tw-mt-8 md:tw-mt-10">
-      <div class="tw-uppercase lg:tw-w-1/2 tw-text-gray-800 md:tw-tracking-wide tw-font-serif md:tw-text-center lg:tw-text-left tw-text-4xl sm:tw-text-6xl tw-font-thin md:tw-text-7xl">
-        belzainco global links nigerian limited
-        <p class="md:tw-text-lg tw-mr-2 tw-text-sm tw-text-gray-700 tw-tracking-normal tw-font-thin tw-ml-1 tw-mt-4 tw-font-mono">
-          Belzainco global links nigerian limited offers the best service in sourcing, storage and transporting agricultural products.
-          our role is to ensure that we import/export of a raw quality agricultural products at affordable price to our customers.
-        </p>
-      </div>
-      <div class="lg:tw-w-1/2 tw-mt-6 md:tw-mt-0">
-        <img class="tw-w-full tw-opacity-100 hover:tw-opacity-80 tw-object-cover tw-rounded-3xl tw-shadow-xl tw-h-full hover:tw-shadow-2xl" src="/img/pexels-wendy-wei-1656666.jpg" alt="">
-      </div>
+  <div class="lg:tw-flex tw-justify-between tw-w-5/6 tw-mx-auto tw-mt-8 md:tw-mt-10">
+    <div class="tw-uppercase lg:tw-w-1/2 tw-text-gray-800 md:tw-tracking-wide tw-font-serif md:tw-text-center lg:tw-text-left tw-text-4xl sm:tw-text-6xl tw-font-thin md:tw-text-7xl">
+      belzainco global links nigerian limited
+      <p class="md:tw-text-lg tw-mr-2 tw-text-sm tw-text-gray-700 tw-tracking-normal tw-font-thin tw-ml-1 tw-mt-4 tw-font-mono">
+        Belzainco global links nigerian limited offers the best service in sourcing, storage and transporting agricultural products.
+        our role is to ensure that we import and export a raw quality agricultural products at affordable price to our customers.
+      </p>
+    </div>
+    <div class="lg:tw-w-1/2 tw-mt-6 md:tw-mt-0">
+      <img class="tw-w-full tw-opacity-100 hover:tw-opacity-80 tw-object-cover tw-rounded-3xl tw-shadow-xl tw-h-full hover:tw-shadow-2xl" src="/img/pexels-wendy-wei-1656666.jpg" alt="">
     </div>
   </div>
 </template>
