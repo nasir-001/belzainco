29e442cf43b2856e8ce80451af9e3ee7a703be5f
Nasir <nasirlawal001@gmail.com>
2021-06-26 12:26:21 +0100

    change the products card

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -3,18 +3,18 @@
     <div class="tw-text-2xl sm:tw-text-3xl md:tw-text-4xl tw-text-center tw-text-gray-800 tw-font-mono">
       Our Products
     </div>
-    <div class="tw-flex tw-justify-evenly tw-mx-28 tw-mt-6 tw-mb-10">
-      <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
-        <img class="tw-object-cover tw-opacity-100 hover:tw-opacity-80 tw-h-52 tw-w-full"  src="/img/pexels-ready-made-3987286.jpg" alt="">
+    <div class="tw-flex tw-mt-20 tw-justify-evenly tw-mx-28 tw-mb-10">
+      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
+        <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-naman-nayar-922764.jpg" alt="">
       </div>
-      <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
-        <img class="tw-object-cover tw-opacity-100 hover:tw-opacity-80 tw-h-52 tw-w-full"  src="/img/pexels-ready-made-3850652.jpg" alt="">
+      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
+        <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-naman-nayar-922764.jpg" alt="">
       </div>
-      <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
-        <img class="tw-object-cover tw-opacity-100 hover:tw-opacity-80 tw-h-52 tw-w-full"  src="/img/pexels-pixabay-39303.jpg" alt="">
+      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
+        <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-naman-nayar-922764.jpg" alt="">
       </div>
-      <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
-        <img class="tw-object-cover tw-opacity-100 hover:tw-opacity-80 tw-h-52 tw-w-full"  src="/img/pexels-naman-nayar-922764.jpg" alt="">
+      <div class="tw-bg-gray-50 tw-shadow-xl tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-h-60 tw-mb-10 tw-w-1/4 tw-rounded-3xl tw-mx-2">
+        <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-naman-nayar-922764.jpg" alt="">
       </div>
     </div>
   </div>
