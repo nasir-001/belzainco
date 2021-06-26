77a0e92c7ef0aabed1bfb3c85c3e71e6b86140ee
Nasir <nasirlawal001@gmail.com>
2021-06-26 08:22:33 +0100

    add company aims to the overview component

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="tw-mx-0">
+  <div>
     <div class="lg:tw-flex tw-justify-between tw-w-5/6 tw-mx-auto tw-mt-8 md:tw-mt-10">
-      <div class="tw-uppercase lg:tw-w-1/2 tw-text-gray-800 md:tw-tracking-wide tw-font-serif tw-text-center lg:tw-text-left tw-text-4xl sm:tw-text-6xl tw-font-thin md:tw-text-7xl">
+      <div class="tw-uppercase lg:tw-w-1/2 tw-text-gray-800 md:tw-tracking-wide tw-font-serif md:tw-text-center lg:tw-text-left tw-text-4xl sm:tw-text-6xl tw-font-thin md:tw-text-7xl">
         belzainco global links nigerian limited
-        <p class="lg:tw-text-md tw-text-lg tw-text-gray-700 tw-tracking-normal tw-font-thin tw-ml-1 tw-mt-4 tw-font-mono">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quo molestiae, vitae dolorum beatae dolores maiores ut architecto cum aperiam fuga. Quo nam maxime veritatis velit animi omnis voluptates! Rem?</p>
+        <p class="md:tw-text-lg tw-text-sm tw-text-gray-700 tw-tracking-normal tw-font-thin tw-ml-1 tw-mt-4 tw-font-mono">
+          Belzainco global links nigerian limited offers the best service in sourcing, storage and transporting agricultural products.
+          our role is to ensure that we import/export of a raw quality agricultural products at affordable price to our customers.
+        </p>
       </div>
       <div class="lg:tw-w-1/2 lg:tw--mt-20">
         <!-- <img class="tw-w-full" src="/img/images.jpg" alt=""> -->
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -5,19 +5,19 @@
     </div>
     <div class="tw-flex tw-justify-evenly tw-mx-28 tw-mt-6 tw-mb-10">
       <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
-        <img class="tw-object-contain tw-opacity-100 hover:tw-opacity-80" src="/img/images.jpg" alt="">
+        <img class="tw-object-contain tw-opacity-100 hover:tw-opacity-80"  src="/img/images.jpg" alt="">
         <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2 hover:tw-text-gray-800 tw-font-mono tw-tracking-wider">mango</p>
       </div>
       <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
-        <img class="tw-object-contain tw-opacity-100 hover:tw-opacity-80" src="/img/images.jpg" alt="">
+        <img class="tw-object-contain tw-opacity-100 hover:tw-opacity-80"  src="/img/images.jpg" alt="">
         <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2 hover:tw-text-gray-800 tw-font-mono tw-tracking-wider">mango</p>
       </div>
       <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
-        <img class="tw-object-contain tw-opacity-100 hover:tw-opacity-80" src="/img/images.jpg" alt="">
+        <img class="tw-object-contain tw-opacity-100 hover:tw-opacity-80"  src="/img/images.jpg" alt="">
         <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2 hover:tw-text-gray-800 tw-font-mono tw-tracking-wider">mango</p>
       </div>
       <div class="tw-w-1/4 tw-mx-2 tw-rounded-3xl tw-shadow-xl tw-overflow-hidden hover:tw-shadow-2xl">
-        <img class="tw-object-contain tw-opacity-100 hover:tw-opacity-80" src="/img/images.jpg" alt="">
+        <img class="tw-object-contain tw-opacity-100 hover:tw-opacity-80"  src="/img/images.jpg" alt="">
         <p class="tw-text-center tw-text-lg tw-text-gray-600 tw-py-2 hover:tw-text-gray-800 tw-font-mono tw-tracking-wider">mango</p>
       </div>
     </div>
