2a36641a790c147fb6e9dca5613fa611238580bb
Nasir <nasirlawal001@gmail.com>
2021-06-27 17:40:49 +0100

    add image to the dropdown button

--- src/components/ServiceCard.vue
+++ src/components/ServiceCard.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <q-btn-dropdown flat label="agro processing" class="md:tw-text-xl tw-text-sm tw-w-full tw-text-gray-700 tw-tracking-widest tw-font-thin tw-font-mono" dropdown-icon="">
+    <q-btn-dropdown flat label="agro processing" class="sm:tw-text-md tw-w-full tw-text-gray-700 tw-tracking-widest tw-font-thin tw-font-mono" dropdown-icon="">
       <q-list>
         <q-item clickable v-close-popup @click="onItemClick">
           <q-item-section>
-            <q-item-label>Photos</q-item-label>
+            <q-item-label class="tw-shadow-xl">
+              <img class="tw-object-cover tw-shadow-xl tw-h-48 tw-rounded-lg tw-overflow-hidden tw-rounded-b-lg hover:tw-shadow-2xl"  src="/img/pexels-pixabay-39304.jpg" alt="">
+            </q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="tw-mx-32 tw-my-10">
+  <div class="tw-mx-2 lg:tw-mx-32 tw-my-10">
     <div class="tw-text-xl sm:tw-text-3xl md:tw-text-4xl tw-text-center tw-text-gray-800 tw-font-mono">
       Service we Provides
-    </div>
-    <div class="tw-flex sm:tw-flex-wrap tw-justify-evenly tw-mt-10">
-      <div class="tw-w-1/4 tw-text-xl">
+    </div> 
+    <div class="tw-flex tw-justify-center md:tw-justify-start tw-flex-wrap tw-mb-10">
+      <div class="tw-w-1/2 md:tw-w-1/3 lg:tw-w-1/4 tw-text-xl tw-mx-0">
         <service-card></service-card>
       </div>
-      <div class="tw-w-1/4 tw-text-xl">
+      <div class="tw-w-1/2 md:tw-w-1/3 lg:tw-w-1/4 tw-text-xl tw-mx-0">
         <service-card></service-card>
       </div>
-      <div class="tw-w-1/4 tw-text-xl">
+      <div class="tw-w-1/2 md:tw-w-1/3 lg:tw-w-1/4 tw-text-xl tw-mx-0">
         <service-card></service-card>
       </div>
-      <div class="tw-w-1/4 tw-text-xl">
+      <div class="tw-w-1/2 md:tw-w-1/3 lg:tw-w-1/4 tw-text-xl tw-mx-0">
         <service-card></service-card>
       </div>
     </div>
