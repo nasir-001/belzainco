c5558652a88d21cfa9dfd4417adbea682756f061
Nasir <nasirlawal001@gmail.com>
2021-06-27 18:07:41 +0100

    resize dropdown image

--- src/components/ServiceCard.vue
+++ src/components/ServiceCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <q-btn-dropdown flat label="agro processing" class="sm:tw-text-md tw-w-full tw-text-gray-700 tw-tracking-widest tw-font-thin tw-font-mono" dropdown-icon="">
+    <q-btn-dropdown flat label="agro processing" class="sm:tw-text-md tw-mt-6 tw-w-full tw-text-gray-700 tw-tracking-widest tw-font-thin tw-font-mono" dropdown-icon="">
       <q-list>
         <q-item clickable v-close-popup @click="onItemClick">
           <q-item-section>
             <q-item-label class="tw-shadow-xl">
-              <img class="tw-object-cover tw-shadow-xl tw-h-48 tw-rounded-lg tw-overflow-hidden tw-rounded-b-lg hover:tw-shadow-2xl"  src="/img/pexels-pixabay-39304.jpg" alt="">
+              <img class="tw-object-cover tw-shadow-xl tw-h-48 tw-rounded-lg tw-overflow-hidden tw-rounded-b-lg tw-mx-auto tw-w-full hover:tw-shadow-2xl"  src="/img/pexels-pixabay-39304.jpg" alt="">
             </q-item-label>
           </q-item-section>
         </q-item>
