bc818d19c7a901bbafdf2d75bed1550d40344773
Nasir <nasirlawal001@gmail.com>
2021-06-28 22:59:33 +0100

    start implementing the about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
+  <div class="tw-uppercase tw-mx-28 tw-text-gray-800 tw-mt-10 md:tw-tracking-widest tw-font-serif tw-text-center md:tw-text-left tw-text-3xl sm:tw-text-4xl tw-font-thin md:tw-text-5xl">
     About Us
+  </div>
+  <div class="tw-flex tw-flex-wrap tw-justify-center">
+    <div class="md:tw-text-lg tw-w-3/6 tw-mx-4 tw-text-sm tw-text-gray-700 tw-tracking-normal tw-font-thin tw-mt-4 tw-font-mono">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure consequuntur, aspernatur cum necessitatibus nisi earum praesentium! Assumenda maiores soluta veritatis? Ratione eum sunt quae ea debitis. Fugiat ducimus sequi et!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium fugit aut vitae beatae, iure voluptatibus. Nemo laborum error veniam dicta! Ea nisi obcaecati in ipsum est. Error magni odio animi.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, qui reiciendis repudiandae vel omnis vero ipsa maxime deserunt magnam sed minus dolorem eveniet rerum. Quia nulla beatae suscipit nisi omnis.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae eos provident illo debitis sequi ex rerum numquam explicabo, similique aliquid. Repudiandae porro fugiat explicabo dignissimos! Eaque laudantium harum dicta enim.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure consequuntur, aspernatur cum necessitatibus nisi earum praesentium! Assumenda maiores soluta veritatis? Ratione eum sunt quae ea debitis. Fugiat ducimus sequi et!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium fugit aut vitae beatae, iure voluptatibus. Nemo laborum error veniam dicta! Ea nisi obcaecati in ipsum est. Error magni odio animi.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, qui reiciendis repudiandae vel omnis vero ipsa maxime deserunt magnam sed minus dolorem eveniet rerum. Quia nulla beatae suscipit nisi omnis.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae eos provident illo debitis sequi ex rerum numquam explicabo, similique aliquid. Repudiandae porro fugiat explicabo dignissimos! Eaque laudantium harum dicta enim.
+    </div>
+    <div class="tw-w-2/6">
+      <img class="tw-w-full tw-opacity-100  hover:tw-opacity-80 tw-object-cover tw-rounded-3xl tw-shadow-xl tw-h-full hover:tw-shadow-2xl" src="/img/pexels-wendy-wei-1656666.jpg" alt="">
+    </div>
   </div>
 </template>
 
