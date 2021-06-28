df7551d73728619053680aae06613491bd660030
Nasir <nasirlawal001@gmail.com>
2021-06-28 21:03:07 +0100

    edit badge for product name

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -6,19 +6,19 @@
     <div class="tw-flex tw-flex-wrap tw-mt-20 tw-justify-start sm:tw-justify-center tw-mb-10">
       <div class="sm:tw-mx-2 tw-bg-gray-50 tw-shadow-xl tw-mb-20 tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-border-2 tw-h-60 tw-w-1/2 sm:tw-w-1/3 md:tw-w-1/4 lg:tw-w-1/5 tw-rounded-3xl">
         <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-ready-made-3987286.jpg" alt="">
-        <q-badge outline label="Mango" class="tw-ml-6 tw-mt-4 tw-text-sm tw-text-pink-500 tw-px-3" />
+        <q-badge outline label="Mango" class="tw-ml-6 tw-mt-4 tw-text-sm tw-text-gray-500 tw-font-mono tw-font-thin tw-px-3" />
       </div>
       <div class="sm:tw-mx-2 tw-bg-gray-50 tw-shadow-xl tw-mb-20 tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-border-2 tw-h-60 tw-w-1/2 sm:tw-w-1/3 md:tw-w-1/4 lg:tw-w-1/5 tw-rounded-3xl">
         <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-ready-made-3850652.jpg" alt="">
-        <q-badge outline label="Avocado" class="tw-ml-6 tw-mt-4 tw-text-sm tw-text-pink-500 tw-px-3" />
+        <q-badge outline label="Avocado" class="tw-ml-6 tw-mt-4 tw-text-sm tw-text-gray-500 tw-font-mono tw-font-thin tw-px-3" />
       </div>
       <div class="sm:tw-mx-2 tw-bg-gray-50 tw-shadow-xl tw-mb-20 tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-border-2 tw-h-60 tw-w-1/2 sm:tw-w-1/3 md:tw-w-1/4 lg:tw-w-1/5 tw-rounded-3xl">
         <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-castorly-stock-3682190.jpg" alt="">
-        <q-badge outline label="Sesame Seeds" class="tw-ml-6 tw-mt-4 tw-text-sm tw-text-pink-500 tw-px-3" />
+        <q-badge outline label="Sesame Seeds" class="tw-ml-6 tw-mt-4 tw-text-sm tw-text-gray-500 tw-font-mono tw-font-thin tw-px-3" />
       </div>
       <div class="sm:tw-mx-2 tw-bg-gray-50 tw-shadow-xl tw-mb-20 tw-border-gray-200 hover:tw-shadow-2xl hover:tw-opacity-80 tw-border-2 tw-h-60 tw-w-1/2 sm:tw-w-1/3 md:tw-w-1/4 lg:tw-w-1/5 tw-rounded-3xl">
         <img class="tw-object-cover tw-shadow-xl tw-h-48 tw--mt-12 tw-w-5/6 tw-mx-auto hover:tw-shadow-2xl tw-rounded-3xl tw-overflow-hidden"  src="/img/pexels-naman-nayar-922764.jpg" alt="">
-        <q-badge outline label="Hibiscus flower" class="tw-ml-6 tw-mt-4 tw-text-sm tw-text-pink-500 tw-px-3" />
+        <q-badge outline label="Hibiscus flower" class="tw-ml-6 tw-mt-4 tw-text-sm tw-text-gray-500 tw-font-mono tw-font-thin tw-px-3" />
       </div>
     </div>
   </div>
