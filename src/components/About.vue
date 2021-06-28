ee12a292202f456159a15b3570e2403d62e24086
Nasir <nasirlawal001@gmail.com>
2021-06-28 19:25:32 +0100

    create about component

---
+++ src/components/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    About Us
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'About'
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,10 @@ const routes = [
     path: '/',
     component: () => import('layouts/Index.vue')
   },
+  {
+    path: '/about',
+    component: () => import('components/About.vue')
+  },
 
   // Always leave this as last one,
   // but you can also remove it
