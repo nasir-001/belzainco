c9f5d1224a4f1aafa7f78c173623e5659eb77a1e
Nasir <nasirlawal001@gmail.com>
2021-06-29 23:21:56 +0100

    add all footer needed item

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,29 +1,51 @@
 <template>
-  <div class="tw-bottom-0 tw-bg-gray-100">
+  <div class="tw-bottom-0 tw-bg-gray-50">
     <div class="tw-flex tw-flex-wrap tw-justify-center tw-mx-20">
       <div class="tw-mt-10 tw-w-1/3">
-        <div class="tw-text-4xl tw-text-gray-800 hover:tw-text-gray-600 tw-font-sans">
+        <div class="tw-text-4xl tw-text-gray-700 tw-cursor-pointer hover:tw-text-gray-600 tw-font-mono">
           Belzainco
         </div>
-        <div class="tw-text-sm tw-mt-3 tw-text-gray-500 tw-font-serif tw-font-thin">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ad esse repellendus consequatur quidem doloribus aliquid accusamus soluta rem voluptates optio accusantium possimus sapiente numquam nulla quis amet, magnam at?
+        <div class="tw-text-sm tw-mt-3 tw-tracking-widest tw-text-gray-500 tw-font-mono tw-font-thin">
+          Belzainco global links nigerian limited offers the best service in sourcing, storage and transporting agricultural products. our role is to ensure that we import and export a raw quality agricultural products at affordable price to our customers.
         </div>
-        <div class="tw-flex tw-flex-wrap">
-          <q-btn flat dense round icon="phone">
-            <q-item clickable>
-              <q-item-section class="tw-text-sm tw-cursor-pointer tw-uppercase tw-text-gray-800 hover:tw-text-gray-600">
-                <a href="mailto:belzainco.info@yahoo.com">Contact Us</a>
-              </q-item-section>
-            </q-item>
-          </q-btn>
+      </div>      
+      <div class="tw-mt-10 tw-w-1/3">
+        <div class="tw-text-2xl tw-ml-16 tw-text-gray-800 tw-cursor-pointer hover:tw-text-gray-600 tw-font-mono">
+          Services
         </div>
-      </div>
-      <div class="tw-text-3xl tw-mt-10 tw-w-1/3 tw-text-center tw-text-gray-800 hover:tw-text-gray-600 tw-font-sans">
-        Belzainco
-      </div>
-      <div class="tw-text-3xl tw-mt-10 tw-w-1/3 tw-text-center tw-text-gray-800 hover:tw-text-gray-600 tw-font-sans">
-        Belzainco
-      </div>
+        <div class="tw-mt-3 tw-tracking-widest tw-ml-16 tw-text-gray-500 tw-font-mono">
+          Agro processing
+        </div>
+        <q-separator class="tw-mx-14" />
+        <div class="tw-mt-1 tw-tracking-widest tw-ml-16 tw-text-gray-500 tw-font-mono">
+          Agro import/export
+        </div>
+        <q-separator class="tw-mx-14" />
+        <div class="tw-mt-1 tw-tracking-widest tw-ml-16 tw-text-gray-500 tw-font-mono">
+          Aggregators
+        </div>
+        <q-separator class="tw-mx-14" />
+        <div class="tw-mt-1 tw-tracking-widest tw-ml-16 tw-text-gray-500 tw-font-mono">
+          Aggregators
+        </div>
+        <q-separator class="tw-mx-14" />
+        <div class="tw-mt-1 tw-tracking-widest tw-ml-16 tw-text-gray-500 tw-font-mono">
+          Aggregators
+        </div>
+        <q-separator class="tw-mx-14" />
+      </div>      
+      <div class="tw-mt-10 tw-w-1/3">
+        <div class="tw-text-2xl tw-ml-1 tw-text-gray-800 tw-cursor-pointer hover:tw-text-gray-600 tw-font-mono">
+          Contact Us
+        </div>
+        <div class="tw-text-sm tw-mt-3 tw-text-gray-500 tw-font-mono tw-font-thin">
+          <q-btn dense flat rounded icon="phone"></q-btn>  08066241131
+          <q-separator class="tw-w-40" />
+          <q-btn dense flat rounded icon="phone"></q-btn>  08063269348
+          <q-separator class="tw-w-40" />
+          <q-btn dense flat rounded icon="email"></q-btn><a class="tw-ml-2" href="mailto:belzainco.info@yahoo.com">belzainco.info@yahoo.com</a>
+        </div>
+      </div>      
     </div>
   </div>
 </template>
