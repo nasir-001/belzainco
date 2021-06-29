8eebe4165c4d5368b9380930a95d714d6339e8b1
Nasir <nasirlawal001@gmail.com>
2021-06-29 18:36:59 +0100

    add flex to the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="tw-bottom-0 tw-bg-gray-100">
-    <div class="tw-flex tw-flex-wrap tw-justify-evenly">
-      <div class="tw-text-3xl tw-mt-10 tw-text-gray-800 hover:tw-text-gray-600 tw-font-sans">
+    <div class="tw-flex tw-flex-wrap tw-justify-center tw-mx-20">
+      <div class="tw-mt-10 tw-w-1/3">
+        <div class="tw-text-4xl tw-text-gray-800 hover:tw-text-gray-600 tw-font-sans">
+          Belzainco
+        </div>
+        <div class="tw-text-sm tw-mt-3 tw-text-gray-500 tw-font-serif tw-font-thin">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ad esse repellendus consequatur quidem doloribus aliquid accusamus soluta rem voluptates optio accusantium possimus sapiente numquam nulla quis amet, magnam at?
+        </div>
+        <div class="tw-flex tw-flex-wrap">
+          <q-btn flat dense round icon="phone">
+            <q-item clickable>
+              <q-item-section class="tw-text-sm tw-cursor-pointer tw-uppercase tw-text-gray-800 hover:tw-text-gray-600">
+                <a href="mailto:belzainco.info@yahoo.com">Contact Us</a>
+              </q-item-section>
+            </q-item>
+          </q-btn>
+        </div>
+      </div>
+      <div class="tw-text-3xl tw-mt-10 tw-w-1/3 tw-text-center tw-text-gray-800 hover:tw-text-gray-600 tw-font-sans">
         Belzainco
       </div>
-      <div class="tw-text-3xl tw-mt-10 tw-text-gray-800 hover:tw-text-gray-600 tw-font-sans">
-        Belzainco
-      </div>
-      <div class="tw-text-3xl tw-mt-10 tw-text-gray-800 hover:tw-text-gray-600 tw-font-sans">
-        Belzainco
-      </div>
-      <div class="tw-text-3xl tw-mt-10 tw-text-gray-800 hover:tw-text-gray-600 tw-font-sans">
+      <div class="tw-text-3xl tw-mt-10 tw-w-1/3 tw-text-center tw-text-gray-800 hover:tw-text-gray-600 tw-font-sans">
         Belzainco
       </div>
     </div>
