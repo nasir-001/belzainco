e95420c2db21071a5721074c452b779a97f60c19
Nasir <nasirlawal001@gmail.com>
2021-06-29 09:16:05 +0100

    change button color

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tw-uppercase tw-mx-28 tw-text-gray-800 tw-mt-10 md:tw-tracking-widest tw-font-serif tw-text-center md:tw-text-left tw-text-3xl sm:tw-text-4xl tw-font-thin md:tw-text-5xl">
+  <div class="tw-uppercase md:tw-mx-28 tw-text-gray-800 tw-mt-10 md:tw-tracking-widest tw-font-serif tw-text-center md:tw-text-left tw-text-3xl sm:tw-text-4xl tw-font-thin md:tw-text-5xl">
     About Us
   </div>
   <div class="tw-flex tw-flex-wrap tw-justify-center">
@@ -9,9 +9,7 @@
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, qui reiciendis repudiandae vel omnis vero ipsa maxime deserunt magnam sed minus dolorem eveniet rerum. Quia nulla beatae suscipit nisi omnis.
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae eos provident illo debitis sequi ex rerum numquam explicabo, similique aliquid. Repudiandae porro fugiat explicabo dignissimos! Eaque laudantium harum dicta enim.
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure consequuntur, aspernatur cum necessitatibus nisi earum praesentium! Assumenda maiores soluta veritatis? Ratione eum sunt quae ea debitis. Fugiat ducimus sequi et!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium fugit aut vitae beatae, iure voluptatibus. Nemo laborum error veniam dicta! Ea nisi obcaecati in ipsum est. Error magni odio animi.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, qui reiciendis repudiandae vel omnis vero ipsa maxime deserunt magnam sed minus dolorem eveniet rerum. Quia nulla beatae suscipit nisi omnis.
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae eos provident illo debitis sequi ex rerum numquam explicabo, similique aliquid. Repudiandae porro fugiat explicabo dignissimos! Eaque laudantium harum dicta enim.
+      
     </div>
     <div class="tw-w-2/6">
       <img class="tw-w-full tw-opacity-100  hover:tw-opacity-80 tw-object-cover tw-rounded-3xl tw-shadow-xl tw-h-full hover:tw-shadow-2xl" src="/img/pexels-wendy-wei-1656666.jpg" alt="">
