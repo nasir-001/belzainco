b7a8d7b08d2783f3c3f85295c46606ee51359c27
Nasir <nasirlawal001@gmail.com>
2021-07-03 11:05:49 +0100

    edit grammatical typo

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tw-mx-2 lg:tw-mx-32 tw-my-10">
     <div class="tw-text-xl sm:tw-text-3xl md:tw-text-4xl tw-text-center tw-text-gray-800 tw-font-mono">
-      Service we Provides
+      Service We Provide
     </div> 
     <div class="tw-flex tw-justify-center md:tw-justify-start tw-flex-wrap tw-mb-10">
       <div class="tw-w-full sm:tw-w-1/2 md:tw-w-1/3 lg:tw-w-1/4 tw-text-xl tw-mx-0">
